0f4cde5d3fd1623b349724300b62a1c5f89c98d0
tcg <you@example.comkwanelemkhwanazi688@gmail.com>
2021-09-29 09:32:55 +0200

    added a src folder on the frontend folder

--- tcg-app/frontend/src/App.js
+++ tcg-app/frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom';
+import Nav from './components/Nav';
+import Form from './components/Form';
+import Home from './components/Home';
+import UpdateForm from './components/UpdateForm';
 
 function App() {
   return (
+    <Router >
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      hello
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={Form} />
+      <Route path="/main" component={UpdateForm} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
