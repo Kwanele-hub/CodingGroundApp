e92e5539356314523d4fd63d800d1bcdf6493e9d
tcg <you@example.comkwanelemkhwanazi688@gmail.com>
2021-09-30 14:25:32 +0200

    added cors

--- tcg-app/backend/server.js
+++ tcg-app/backend/server.js
@@ -1,22 +1,30 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.KWANELE_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection= mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongodb database connection  successfully !!");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 
-app.use(cors())
-app.use(express.json())
-mongoose.connect(
-    "mongodb+srv://kwanele:sphe1906@cluster0.wrcxy.mongodb.net/TCG-app?retryWrites=true&w=majority",
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    },
-
-    () => {
-        console.log("mongobd database connected!")
-    }
-);
-route(app)
-
-const port = 5100
-app.listen(port, () => console.log(`server running on port: ${port}`))
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
